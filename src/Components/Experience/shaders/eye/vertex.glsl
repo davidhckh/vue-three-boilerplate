05ba333b1644215619a2dbfb9e80f7063d0fa497
davidhckh <david.heckhoff@gmail.com>
2022-12-14 12:42:57 +0100

    chore: delete shaders

--- src/Components/Experience/shaders/eye/vertex.glsl
+++
@@ -1,9 +0,0 @@
-varying vec2 vUv;
-varying float vDistToCamera;
-
-void main() {
-    gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
-
-    vUv = uv;
-    vDistToCamera = gl_Position.w;
-}
